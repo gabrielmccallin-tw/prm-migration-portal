7c1f11c642ec7def43bc726b88d45cfd5e2f28db
gabrielmccallin-tw <gabriel.mccallin@thoughtworks.com>
2019-12-17 16:21:42 +0000

    [GM] Parth port

--- migration-portal/src/env.js
+++ migration-portal/src/env.js
@@ -1,5 +1,5 @@
 export const publicPath = 'prm-migration-portal';
-export const stubApiDomain = 'http://localhost:5000';
+export const stubApiDomain = 'http://localhost:5001';
 export const apiDomain = 'http://deployed.com/migration:5000';
 export const endpoints = {
     patients: 'patients',
