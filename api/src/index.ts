4f85b4c79db02ede586c2261cacf82e10c6bd5f6
gabrielmccallin-tw <gabriel.mccallin@thoughtworks.com>
2019-12-20 17:17:28 +0000

    [GM] More realistic dates

--- api/src/index.ts
+++ api/src/index.ts
@@ -26,35 +26,35 @@ const data = [{
     practice: 'GP Practice 1',
     telephone: '07700 900457',
     email: 'richard.grayson@example.com',
-    dob: '5 January 1978',
+    dob: '1978-01-05T00:00:00',
     nhsNumber: '1234567890'
 }, {
     patientName: 'Rachel Roth',
     practice: 'GP Practice 2',
     telephone: '07700 900457',
     email: 'rachel.roth@example.com',
-    dob: '5 January 1978',
+    dob: '1978-01-05T00:00:00',
     nhsNumber: '2234567890'
 }, {
     patientName: 'Donna Troy',
     practice: 'GP Practice 3',
     telephone: '07700 900457',
     email: 'donna.troy@example.com',
-    dob: '5 January 1978',
+    dob: '1978-01-05T00:00:00',
     nhsNumber: '3234567890'
 }, {
     patientName: 'Kory Anders',
     practice: 'GP Practice 4',
     telephone: '07700 900457',
     email: 'Kory.Anders@example.com',
-    dob: '5 January 1978',
+    dob: '1978-01-05T00:00:00',
     nhsNumber: '4234567890'
 }, {
     patientName: 'Garfield Logan',
     practice: 'GP Practice 5',
     telephone: '07700 900457',
     email: 'garfield.logan@example.com',
-    dob: '5 January 1978',
+    dob: '1978-01-05T00:00:00',
     nhsNumber: '5234567890'
 }];
 
@@ -74,13 +74,12 @@ app.get(`/${endpoints.patient}/:nhs`, (req, res) => {
 
 app.post(`/${endpoints.patients}`, (req, res) => {
     const { patientName, requester, nhsNumber } = req.body;
-    console.log(req.body);
     const patient = {
         patientName,
         requester,
         nhsNumber,
         status: 'Success',
-        requestDate: 'Today'
+        requestDate: '2019-12-05T00:00:00'
     };
     patients.push(patient);
 
